6f03e18bb0f1d91e0e4d91e6dc7bc0facdfceed3
James Leslie <contactjamesleslie@gmail.com>
2023-05-30 17:48:43 +0200

    Renamed and moved to subfolder

--- src/fpl_data/api.py
+++ src/fpl_data/api.py
@@ -1,39 +1,36 @@
 import requests
-import time
-from tqdm.auto import tqdm
+# import time
+# from tqdm.auto import tqdm
+from fpl_data.utils import drop_keys
+
 
 BASE_URL = 'https://fantasy.premierleague.com/api/'
 
 
-def drop_keys(d, keys):
-    '''Drop keys from a dictionary'''
-    return {k: v for k, v in d.items() if k not in keys}
+# def get_element_summary(player_id, type):
+#     '''Get all past season info for a given player_id,
+#        wait between requests to avoid API rate limit'''
 
+#     success = False
+#     # try until a result is returned
+#     while not success:
+#         try:
+#             # send GET request to BASE_URL/api/element-summary/{PID}/
+#             data = requests.get(
+#                 BASE_URL + 'element-summary/' + str(player_id) + '/').json()
+#             success = True
+#         except:
+#             # wait a bit to avoid API rate limits, if needed
+#             time.sleep(.3)
 
-def get_element_summary(player_id, type):
-    '''Get all past season info for a given player_id,
-       wait between requests to avoid API rate limit'''
+#     # extract 'history_past' data from response into dataframe
+#     df = pd.json_normalize(data[type])
 
-    success = False
-    # try until a result is returned
-    while not success:
-        try:
-            # send GET request to BASE_URL/api/element-summary/{PID}/
-            data = requests.get(
-                BASE_URL + 'element-summary/' + str(player_id) + '/').json()
-            success = True
-        except:
-            # wait a bit to avoid API rate limits, if needed
-            time.sleep(.3)
+#     # season history needs player id column added
+#     if type == 'history_past':
+#         df.insert(0, 'id', player_id)
 
-    # extract 'history_past' data from response into dataframe
-    df = pd.json_normalize(data[type])
-
-    # season history needs player id column added
-    if type == 'history_past':
-        df.insert(0, 'id', player_id)
-
-    return df
+#     return df
 
 
 class FplApiData:
